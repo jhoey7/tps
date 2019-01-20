ff8fe499938e5c59c3e4f7cb8c797518e2ee0719
joni.i7 <jhoey.potter7@gmail.com>
2019-01-20 13:07:32 +0700

    Perubahan Untuk Change Password Setelah Login

--- app/views/content/home/password.php
+++ app/views/content/home/password.php
@@ -1,23 +1,43 @@
-<div class="page animsition vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
-    <div class="page-content vertical-align-middle" style="background:#000;opacity:0.7">
-      <div class="avatar avatar-lg">
-        <img src="<?php echo ($this->session->userdata('PATH')!="")?base_url().$this->session->userdata('PATH'):base_url().'assets/images/user.png'; ?>" alt="...">
-      </div>
-      <p class="locked-user"><?php echo $this->session->userdata('NM_LENGKAP'); ?></p>
-      <form name="form_data" id="form_data" role="form" action="<?php echo site_url('home/execute/update/password'); ?>" method="post" autocomplete="off" onsubmit="signin('form_data'); return false;">
-      	<div class="form-group form-material floating">
-          <input type="password" class="form-control" name="DATA[PASS_OLD]" id="PASS_OLD" mandatory="yes" placeholder="PASSWORD LAMA" style="text-transform:none">
-        </div>
-        <div class="form-group form-material floating">
-          <input type="password" class="form-control" name="DATA[PASS_NEW]" id="PASS_NEW" mandatory="yes" placeholder="PASSWORD BARU" style="text-transform:none">
-        </div>
-        <div class="form-group form-material floating">
-          <input type="password" class="form-control" name="DATA[PASS_CONFIRM]" id="PASS_CONFIRM" mandatory="yes" placeholder="KONFIRMASI PASSWORD BARU" style="text-transform:none">
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">SIGN IN</button>
-      </form>
-      <footer class="page-copyright page-copyright-inverse">
-      	<p>&copy; 2016. All RIGHT RESERVED.</p>
-    </footer>
-    </div>
-  </div>
+<section>
+  <div class="panel panel-signin">
+      <div class="panel-body">
+          <div class="logo text-center">
+              <img src="<?php echo ($this->session->userdata('PATH')!="")?base_url().$this->session->userdata('PATH'):base_url().'assets/images/user.png'; ?>" alt="..." width="50px;">
+          </div>
+          <br />
+          <p class="locked-user"><?php echo $this->session->userdata('NM_LENGKAP'); ?></p>
+          <div class="mb30"></div>
+          <form name="form_data" id="form_data" role="form" action="<?php echo site_url('home/execute/update/password'); ?>" method="post" autocomplete="off" onsubmit="signin('form_data'); return false;">
+              <div class="input-group mb15">
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                  <input type="password" class="form-control" name="DATA[PASS_OLD]" id="PASS_OLD" mandatory="yes" placeholder="PASSWORD LAMA">
+              </div><!-- input-group -->
+              <div class="input-group mb15">
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                  <input type="password" class="form-control" name="DATA[PASS_NEW]" id="PASS_NEW" mandatory="yes" placeholder="PASSWORD BARU">
+              </div><!-- input-group -->
+              <div class="input-group mb15">
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                  <input type="password" class="form-control" name="DATA[PASS_CONFIRM]" id="PASS_CONFIRM" mandatory="yes" placeholder="KONFIRMASI PASSWORD BARU">
+              </div><!-- input-group -->
+              
+              <div class="clearfix">
+                  <!-- <div class="pull-left">
+                      <div class="ckbox ckbox-primary mt10">
+                          <input type="checkbox" id="rememberMe" value="1">
+                          <label for="rememberMe">Remember Me</label>
+                      </div>
+                  </div> -->
+                  <div class="pull-right">
+                      <button type="submit" class="btn btn-success">Sign In <i class="fa fa-angle-right ml5"></i></button>
+                  </div>
+              </div>                      
+          </form>
+          
+      </div><!-- panel-body -->
+      <div class="panel-footer">
+          <p>&copy; 2019. All RIGHT RESERVED.</p>
+      </div><!-- panel-footer -->
+  </div><!-- panel -->
+  
+</section>
